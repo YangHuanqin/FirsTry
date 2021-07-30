2f61929b402cf9b7f374911fb19ce3b42e54b690
YangHuanqin <2857842743@qq.com>
2021-07-31 01:58:06 +0800

    first commit.

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 杨焕钦 on 2021/7/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    private lazy var background_up:UIImageView={
+//        let imageView = UIImageView(image: UIImage(named: "bg_up"))
+//        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+//        return imageView
+//    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        self.background_up.frame=CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+//
+//        self.view.addSubview(self.background_up)
+    }
+
+
+}
+
